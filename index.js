3f2fda67cbb817b4f5044890c863711a2b3e9087
Berat Dilki <bd154494@renishaw.com>
2024-10-28 11:28:57 +0000

    complete

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
+
+async function getImage(query) {
+  try {
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=dogs&limit=25&offset=0&rating=g&lang=en`;
+
+    const response = await fetch(endpoint);
+
+    const data = await response.json();
+
+    const imageUrl = data.data[0]?.images?.original?.url;
+
+    console.log(`GIF URL: ${imageUrl}`);
+
+    return imageUrl;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+console.log(process.env.API_KEY);
